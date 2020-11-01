463e45ee6975888df4d54fec91d16c036a54df4a
Teemu Mäenpää <teemumaen@gmail.com>
2020-11-01 15:01:16 +0200

    main ja kasittele_hiiri lisätty

--- miinaharava_ikkuna.py
+++ miinaharava_ikkuna.py
@@ -1,1 +1,13 @@
-
+import haravasto
+def main():
+    """
+    Luo sovellusikkunan ja asettaa käsittelijäfunktion hiiren klikkauksille.
+    Käynnistää sovelluksen.
+    """
+def kasittele_hiiri(x, y, nappi, painetut):
+    """
+    Tätä funktiota kutsutaan kun käyttäjä klikkaa sovellusikkunaa hiirellä.
+    Tulostaa hiiren sijainnin sekä painetun napin terminaaliin.
+    """
+if __name__ == "__main__":
+    main()
